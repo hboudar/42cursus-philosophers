f6e6ae2a4d04c9d79922acda827beb56c2ad628b
Cloneg7 <boudarhamza12@gmail.com>
2024-06-28 16:05:16 +0100

    add all

--- philo_init.c
+++ philo_init.c
@@ -6,7 +6,7 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 01:22:45 by hboudar           #+#    #+#             */
-/*   Updated: 2024/06/28 01:41:12 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/06/28 15:24:02 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,39 @@
 
 void initialize_philosophers(t_table *table)
 {
-    for (int i = 0; i < table->num_philosophers; i++)
+    int i;
+
+    i = 0;
+    while (i < table->num_philosophers)
     {
         table->philosophers[i].id = i;
         table->philosophers[i].last_meal_time = get_time_in_ms();
         table->philosophers[i].meals_eaten = 0;
         table->philosophers[i].table = table;
         pthread_create(&table->philosophers[i].thread, NULL, philosopher_routine, &table->philosophers[i]);
+        i++;
     }
 }
 
 void initialize_forks(t_table *table)
 {
+    int i;
+    
+    i = 0;
     table->forks = malloc(sizeof(pthread_mutex_t) * table->num_philosophers);
-    for (int i = 0; i < table->num_philosophers; i++)
+    while (i < table->num_philosophers)
     {
         pthread_mutex_init(&table->forks[i], NULL);
+        i++;
     }
 }
 
 void initialize_table(t_table *table, int argc, char **argv)
 {
-    table->num_philosophers = atoi(argv[1]);
-    table->time_to_die = atoi(argv[2]);
-    table->time_to_eat = atoi(argv[3]);
-    table->time_to_sleep = atoi(argv[4]);
+    table->num_philosophers = ft_atoi(argv[1]);
+    table->time_to_die = ft_atoi(argv[2]);
+    table->time_to_eat = ft_atoi(argv[3]);
+    table->time_to_sleep = ft_atoi(argv[4]);
     table->meals_required = (argc == 6) * ft_atoi(argv[5]);
     table->simulation_running = 1;
     table->philosophers = malloc(sizeof(t_philosopher) * table->num_philosophers);
--- philo_utils.c
+++ philo_utils.c
@@ -6,24 +6,11 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 01:09:39 by hboudar           #+#    #+#             */
-/*   Updated: 2024/06/28 01:37:38 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/06/28 15:13:32 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-//first argument should be at least more than 1
-
-//  data race
-//  lock orde violation
-//  deadlock
-//  starvation
-
-//  pthread_mutex_t    mutex;
-//  int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *mutexattr);
-//  int pthread_mutex_lock(pthread_mutex_t *mutex);
-//  int pthread_mutex_unlock(pthread_mutex_t *mutex);
-//  int pthread_mutex_destroy(pthread_mutex_t *mutex);
 
 int check_args(int argc, char *argv[])
 {
@@ -57,6 +44,7 @@ int check_args(int argc, char *argv[])
 long get_time_in_ms(void)
 {
     struct timeval tv;
+
     gettimeofday(&tv, NULL);
     return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
 }
