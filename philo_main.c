b997bf7de159b7da8ac07c86ec6155060251cfa1
Cloneg7 <boudarhamza12@gmail.com>
2024-06-28 01:49:07 +0100

    add all

--- philo_main.c
+++ philo_main.c
@@ -6,7 +6,7 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 12:48:25 by hboudar           #+#    #+#             */
-/*   Updated: 2024/06/28 01:39:29 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/06/28 01:48:59 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,20 @@ int main(int argc, char **argv)
     }
     initialize_table(&table, argc, argv);
 
-    // // Monitor philosophers' life status
-    // while (table.simulation_running)
-    // {
-    //     for (int i = 0; i < table.num_philosophers; i++)
-    //     {
-    //         if (get_time_in_ms() - table.philosophers[i].last_meal_time > table.time_to_die)
-    //         {
-    //             print_status(&table, table.philosophers[i].id, "died");
-    //             table.simulation_running = 0;
-    //             break;
-    //         }
-    //     }
-    //     usleep(1000); // check every 1ms
-    // }
-    // cleanup_table(&table);
+    // Monitor philosophers' life status
+    while (table.simulation_running)
+    {
+        for (int i = 0; i < table.num_philosophers; i++)
+        {
+            if (get_time_in_ms() - table.philosophers[i].last_meal_time > table.time_to_die)
+            {
+                print_status(&table, table.philosophers[i].id, "died");
+                table.simulation_running = 0;
+                break;
+            }
+        }
+        usleep(1000); // check every 1ms
+    }
+    cleanup_table(&table);
     return 0;
 }
