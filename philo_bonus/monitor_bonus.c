4b7b38662210b6bad0d720dc229d3266f80fe9a8
Cloneg7 <boudarhamza12@gmail.com>
2024-08-12 20:08:27 +0100

    added all

--- philo_bonus/monitor_bonus.c
+++ philo_bonus/monitor_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 10:42:22 by hboudar           #+#    #+#             */
-/*   Updated: 2024/08/12 14:44:08 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/08/12 19:59:10 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,20 @@ void	*monitor_routine(void *arg)
 	t_philo	*philo;
 	t_table	*table;
 
-	philo = (t_philo *)arg;
-	table = philo->table;
+	(1) && (philo = (t_philo *)arg, table = philo->table);
 	while (1)
 	{
-		sem_wait(table->death_lock);
-		sem_wait(table->meals_lock);
+		(1) && (sem_wait(table->death_lock), sem_wait(table->meals_lock));
 		if (table->meals_required != -1
 			&& philo->meals_eaten >= table->meals_required)
-			{
-				sem_post(table->meals_lock);
-				break ;
-			}
+		{
+			sem_post(table->meals_lock);
+			break ;
+		}
 		sem_post(table->death_lock);
 		if (time_in_ms() - philo->last_meal >= table->time_to_die)
 		{
-			sem_post(table->meals_lock);
-			sem_wait(table->print_lock);
+			(1) && (sem_post(table->meals_lock), sem_wait(table->print_lock));
 			printf("%llu %i died\n",
 				time_in_ms() - table->start_time, philo->id);
 			exit(EXIT_FAILURE);
