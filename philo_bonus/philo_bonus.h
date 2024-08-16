843227b5512414511075f3147d420eeb9b9f409e
Cloneg7 <boudarhamza12@gmail.com>
2024-08-16 15:18:28 +0100

    done

--- philo_bonus/philo_bonus.h
+++ philo_bonus/philo_bonus.h
@@ -6,7 +6,7 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 01:08:27 by hboudar           #+#    #+#             */
-/*   Updated: 2024/08/14 10:56:38 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/08/16 15:17:24 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,16 @@ typedef struct s_philo
 typedef struct s_table
 {
 	long long	start_time;
-	int			num_philos;
-	int			meals_to_eat;
-	int			time_to_eat;
-	int			time_to_die;
-	int			time_to_sleep;
+	int		num_philos;
+	int		meals_to_eat;
+	int		time_to_eat;
+	int		time_to_die;
+	int		time_to_sleep;
+	t_philo		*philos;
 	sem_t		*forks;
 	sem_t		*print_lock;
 	sem_t		*death_lock;
 	sem_t		*meals_lock;
-	t_philo		*philos;
 }	t_table;
 
 void		ft_error(char *msg);
